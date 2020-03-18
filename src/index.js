f007ef95a2f3130afee0cf815479d07a6a6b9792
nickolayArbuzov <nickarbuzov@yandex.by>
2020-03-18 23:52:25 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = '' + n;
+    let res = '';
+    for (let i=0;i<str.length;i++){
+        if(str[str.length-i-1] == '-'){
+            return res;
+        }
+        res += str[str.length-i-1];
+    }
+    return res;
 }
